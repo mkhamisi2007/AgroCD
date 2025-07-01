eb07058cba186b8a545d1378aa89a099ba1bb934
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-07-01 09:15:46 +0200

    Update app.js

--- example02-src-code/app.js
+++ example02-src-code/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const hostname = '0.0.0.0'; 
 const port = 3000; 
+const port2 = 5000; 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,3 +13,8 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => { 
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+server.listen2(port, hostname, () => { 
+  console.log(`Server running at http://${hostname}:${port2}/`);
+});
+
